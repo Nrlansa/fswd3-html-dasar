52e0be9e391b412796505d61ad35b8c9492b7ab8
Nurul Annisa <nurulyse@gmail.com>
2023-03-23 09:11:37 +0700

    tugas 9

--- pages/javascript/app.js
+++ pages/javascript/app.js
@@ -1,64 +1,77 @@
-const form = document.querySelector(".form");
-const input = document.getElementById("todo-input");
-const todoList = document.getElementById("todo-list");
-
-let todos = JSON.parse(localStorage.getItem("todos")) || [];
-
-renderTodos();
-
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  const text = input.value.trim();
-
-  if (text !== "") {
-    const todo = {
-      id: new Date().getTime(),
-      text,
-      completed: false,
-    };
-    todos.push(todo);
-    input.value = "";
-    saveTodos();
-    renderTodos();
-  }
-});
-
-todoList.addEventListener("click", (event) => {
-  if (event.target.classList.contains("checkbox")) {
-    const todoId = parseInt(event.target.parentElement.getAttribute("data-id"));
-    const todo = todos.find((t) => t.id === todoId);
-    todo.completed = !todo.completed;
-    saveTodos();
-    renderTodos();
-  } else if (event.target.classList.contains("delete")) {
-    const todoId = parseInt(event.target.parentElement.getAttribute("data-id"));
-    todos = todos.filter((t) => t.id !== todoId);
-    saveTodos();
-    renderTodos();
-  }
-});
-
-function renderTodos() {
-  todoList.innerHTML = "";
-  todos.forEach((todo) => {
-    const li = document.createElement("li");
-    li.setAttribute("data-id", todo.id);
-    li.classList.add("todo-item");
-
-    const text = document.createElement("span");
-    text.classList.add("text");
-    text.innerHTML = todo.text;
-    li.appendChild(text);
-
-    const deleteButton = document.createElement("span");
-    deleteButton.classList.add("delete");
-    deleteButton.innerHTML = "&times;";
-    li.appendChild(deleteButton);
-
-    todoList.appendChild(li);
-  });
-}
-
-function saveTodos() {
-  localStorage.setItem("todos", JSON.stringify(todos));
-}
+     // URL untuk mengakses API dari crudcrud.com
+      const apiUrl = "https://crudcrud.com/api/7620adeabd5d4bc2aaa2a7749f3a3245/todos";
+      
+      // Mendapatkan elemen form dan daftar to-do
+      const todoForm = document.getElementById("todo-form");
+      const todoInput = document.getElementById("todo-input");
+      const todoList = document.getElementById("todo-list");
+      
+      // Mendapatkan daftar to-do dari API
+      async function getTodos() {
+        const response = await fetch(apiUrl);
+        const todos = await response.json();
+        return todos;
+      }
+      
+      // Menambahkan to-do ke API
+      async function addTodo(todo) {
+        const response = await fetch(apiUrl, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json"
+          },
+          body: JSON.stringify(todo)
+        });
+        const newTodo = await response.json();
+        return newTodo;
+      }
+      
+      // Menghapus to-do dari API
+      async function deleteTodo(id) {
+        const response = await fetch(`${apiUrl}/${id}`, {
+          method: "DELETE"
+        });
+        const result = await response.json();
+        return result;
+      }
+      
+      // Menampilkan daftar to-do ke dalam elemen ul
+      function renderTodos(todos) {
+        todoList.innerHTML = "";
+        todos.forEach(todo => {
+          const li = document.createElement("li");
+          li.innerText = todo.title;
+          
+          const deleteButton = document.createElement("button");
+          deleteButton.innerText = "Delete";
+          deleteButton.addEventListener("click", async () => {
+            await deleteTodo(todo._id);
+            const updatedTodos = await getTodos();
+            renderTodos(updatedTodos);
+          });
+          
+          li.appendChild(deleteButton);
+          todoList.appendChild(li);
+        });
+      }
+      
+      // Menambahkan event listener untuk form
+      todoForm.addEventListener("submit", async (event) => {
+        event.preventDefault();
+        
+        const todo = {
+          title: todoInput.value,
+          completed: false
+        };
+        
+        await addTodo(todo);
+        todoInput.value = "";
+        
+        const updatedTodos = await getTodos();
+        renderTodos(updatedTodos);
+      });
+      
+      // Menampilkan daftar to-do pertama kali halaman dimuat
+      getTodos().then(todos => {
+        renderTodos(todos);
+      });
